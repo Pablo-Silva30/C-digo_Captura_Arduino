d002a74a308c965ed91162a3c9d93039db31455d
Pablo <pablo.csilva@sptech.school>
2025-12-05 18:37:14 -0300

    Código para usar o arduino e receber os dados da umidade do solo

---
+++ codigo_Arduino.ino
@@ -0,0 +1,16 @@
+const int PINO_SENSOR_UMIDADE_SOLO = A0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PINO_SENSOR_UMIDADE_SOLO, INPUT);
+}
+
+void loop() {
+  int leituraSensor = analogRead(PINO_SENSOR_UMIDADE_SOLO);
+
+  float porcentagemUmidade = 100 - (leituraSensor / 1023.0) * 100;
+
+  Serial.println(porcentagemUmidade);
+
+  delay(2000);
+}
